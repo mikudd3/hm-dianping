5b6da24caeb294a9f9113df80dcf83cbccdb708f
mikudd3 <488607948@qq.com>
2023-06-24 09:45:14 +0800

    实现了发布blog功能

--- src/main/java/com/hmdp/service/IBlogService.java
+++ src/main/java/com/hmdp/service/IBlogService.java
@@ -1,5 +1,6 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,12 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IBlogService extends IService<Blog> {
 
+    /**
+     * 根据blogid返回blog
+     * @param id
+     * @return
+     */
+    Result queryBlogById(Long id);
+
+    Result queryHotBlog(Integer current);
 }
